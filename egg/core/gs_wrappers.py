5b422b41334214ac0d43b21f43bc0988569de469
eugene-kharitonov <kharitonov@fb.com>
2019-09-10 16:36:58 +0200

    Add ST GS for 1 symbol case (#36)

--- egg/core/gs_wrappers.py
+++ egg/core/gs_wrappers.py
@@ -16,28 +16,55 @@ class GumbelSoftmaxWrapper(nn.Module):
     transforms them into a sample from the Gumbel Softmax (GS) distribution; eval-time it returns greedy one-hot encoding
     of the same shape.
 
-    The temperature of the GS distribution can be annealed using `update_temp`.
+    >>> inp = torch.zeros((4, 10)).uniform_()
+    >>> outp = GumbelSoftmaxWrapper(nn.Linear(10, 2))(inp)
+    >>> torch.allclose(outp.sum(dim=-1), torch.ones_like(outp.sum(dim=-1)))
+    True
+    >>> outp = GumbelSoftmaxWrapper(nn.Linear(10, 2), straight_through=True)(inp)
+    >>> torch.allclose(outp.sum(dim=-1), torch.ones_like(outp.sum(dim=-1)))
+    True
+    >>> (max_value, _), (min_value, _) = outp.max(dim=-1), outp.min(dim=-1)
+    >>> (max_value == 1.0).all().item() == 1 and (min_value == 0.0).all().item() == 1
+    True
     """
-    def __init__(self, agent, temperature=1.0, trainable_temperature=False):
+
+    def __init__(self, agent, temperature=1.0, trainable_temperature=False,
+                 straight_through=False):
         """
         :param agent: The agent to be wrapped. agent.forward() has to output log-probabilities over the vocabulary
         :param temperature: The temperature of the Gumbel Softmax distribution
         :param trainable_temperature: If set to True, the temperature becomes a trainable parameter of the model
+        :params straight_through: Whether straigh-through Gumbel Softmax is used
         """
         super(GumbelSoftmaxWrapper, self).__init__()
         self.agent = agent
+        self.straight_through = straight_through
+
         if not trainable_temperature:
             self.temperature = temperature
         else:
-            self.temperature = torch.nn.Parameter(torch.tensor([temperature]), requires_grad=True)
+            self.temperature = torch.nn.Parameter(
+                torch.tensor([temperature]), requires_grad=True)
 
     def forward(self, *args, **kwargs):
         logits = self.agent(*args, **kwargs)
 
-        if self.training:
-            return RelaxedOneHotCategorical(logits=logits, temperature=self.temperature).rsample()
-        else:
+        if not self.training:
             return torch.zeros_like(logits).scatter_(-1, logits.argmax(dim=-1, keepdim=True), 1.0)
+
+        sample = RelaxedOneHotCategorical(
+            logits=logits, temperature=self.temperature).rsample()
+
+        if self.straight_through:
+            size = sample.size()
+            indexes = sample.argmax(dim=-1)
+            hard_sample = torch.zeros_like(sample).view(-1, size[-1])
+            hard_sample.scatter_(1, indexes.view(-1, 1), 1)
+            hard_sample = hard_sample.view(*size)
+
+            sample = sample + (hard_sample - sample).detach()
+
+        return sample
 
 
 class SymbolGameGS(nn.Module):
