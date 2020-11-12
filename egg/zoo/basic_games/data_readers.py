6a865fb44149c04b383c2a2a586c10d98200f1a4
mbaroni <mbaroni@gmail.com>
2020-11-12 09:38:04 +0100

    Finished documenting data_readers.py

--- egg/zoo/basic_games/data_readers.py
+++ egg/zoo/basic_games/data_readers.py
@@ -2,18 +2,19 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 
-# the classes in this file take input data from a text file and convert them to the format
+# These input-data-processing classes take input data from a text file and convert them to the format
 # appropriate for the recognition and discrimination games, so that they can be read by
-# pytorch standard DataLoader: note that the latter requires the data reading classes to support
+# the standard pytorch DataLoader. The latter requires the data reading classes to support
 # a __len__(self) method, returning the size of the dataset, and a __getitem__(self, idx)
-# method, returning the idx-th item in the dataset
+# method, returning the idx-th item in the dataset. We also provide a get_n_features(self) method,
+# returning the dimensionality of the Sender input vector after it is transformed to one-hot format.
 
-# this class takes an input file with a space-delimited attribute-value vector per
-# line, creates a data-frame with the two mandatory fields expected in EGG games, namely sender_input
-# and labels
-# in this case, the two field contain the same information, namely the input attribute-value vector,
-# which is represented as one-hot in sender_input, and in the original integer-based format in
-# labels
+# The AttValRecoDataset class is used in the reconstruction game. It takes an input file with a 
+# space-delimited attribute-value vector per line and  creates a data-frame with the two mandatory 
+# fields expected in EGG games, namely sender_input and labels.
+# In this case, the two fields contain the same information, namely the input attribute-value vectors,
+# represented as one-hot in sender_input, and in the original integer-based format in
+# labels.
 class AttValRecoDataset(Dataset):
     def __init__(self, path,n_attributes,n_values):
         frame = np.loadtxt(path, dtype='S10')
@@ -37,6 +38,14 @@ class AttValRecoDataset(Dataset):
     def __getitem__(self, idx):
         return self.frame[idx]
 
+# The AttValDiscriDataset class, used in the discrimination game takes an input file with a variable 
+# number of period-delimited fields, where all fields but the last represent attribute-value vectors 
+# (with space-delimited attributes). The last field contains the index (counting from 0) of the target 
+# vector.
+# Here, we create a data-frame containing 3 fields: sender_input, labels and receiver_input (these are
+# expected by EGG, the first two mandatorily so).
+# The sender_input corresponds to the target vector (in one-hot format), labels are the indices of the
+# target vector location and receiver_input is a matrix with a row for each input vector (in input order).
 class AttValDiscriDataset(Dataset):
     def __init__(self, path,n_values):
         frame = open(path,'r')
