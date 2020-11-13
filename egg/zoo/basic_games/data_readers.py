c52bf056713d0848e03adccdf80ce033d41ba7fe
mbaroni <mbaroni@gmail.com>
2020-11-13 16:04:20 +0100

    Added copyright info

--- egg/zoo/basic_games/data_readers.py
+++ egg/zoo/basic_games/data_readers.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import torch
 from torch.utils.data import Dataset
 import numpy as np
