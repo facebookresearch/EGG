82076b17e82c8fc37e2678ac1c2dbb4126dbd3c2
mbaroni <mbaroni@gmail.com>
2020-11-13 16:05:50 +0100

    Added copyright info

--- egg/zoo/basic_games/data_generation_scripts/generate_reconstruction_dataset.pl
+++ egg/zoo/basic_games/data_generation_scripts/generate_reconstruction_dataset.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/perl -w
 
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 # This script generates a data set in the format expected by EGG's basic_games reconstruction game.
 
 # Note that the script only generates distinct items: you migt want to sample with replacement from
