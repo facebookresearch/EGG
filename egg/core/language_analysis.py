c4469125f2d022be2997906ebaa0c40513bc1aa1
robertodessi <roberto.dessi11@gmail.com>
2020-12-01 07:35:47 -0800

    fix style

--- egg/core/language_analysis.py
+++ egg/core/language_analysis.py
@@ -316,10 +316,10 @@ class BosDisent(Callback):
     def bosdis(attributes: torch.Tensor, messages, vocab_size) -> float:
         """Miniature language with perfect (=1) bosdis. Taken from Chaabouni et al. 2020, Appendix section 8.2.
         >>> attributes = torch.Tensor(
-        ...     [[0, 0], [0, 1], [0, 2], [0, 3],
-        ...     [1, 0], [1, 1], [1, 2], [1, 3],
-        ...     [2, 0], [2, 1], [2, 2], [2, 3],
-        ...     [3, 0], [3, 1], [3, 2], [3, 3]]
+        ... [[0, 0], [0, 1], [0, 2], [0, 3],
+        ... [1, 0], [1, 1], [1, 2], [1, 3],
+        ... [2, 0], [2, 1], [2, 2], [2, 3],
+        ... [3, 0], [3, 1], [3, 2], [3, 3]]
         ... )
         >>> messages = torch.Tensor(
         ... [[0, 0, 4], [0, 0, 5], [0, 0, 6], [0, 0, 7],
