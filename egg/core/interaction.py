a07b4f36bdb26a9e037312f9431fb2522f75514b
robertodessi <roberto.dessi11@gmail.com>
2020-12-07 08:39:03 -0800

    fix upd

--- egg/core/interaction.py
+++ egg/core/interaction.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import random
 from dataclasses import dataclass
 from typing import Dict, Iterable, Optional
 
