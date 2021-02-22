4f1da1a859c07fd5397bd5f8ebd7a5dd61ad095b
robertodessi <roberto.dessi11@gmail.com>
2021-02-22 04:13:55 -0800

    fix cont_communication

--- egg/core/continous_communication.py
+++ egg/core/continous_communication.py
@@ -3,12 +3,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Callable
+from typing import Callable, Optional
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from egg.core import Interaction
+from egg.core.interaction import LoggingStrategy
 
 
 class ContinuousLinearSender(nn.Module):
@@ -22,13 +23,14 @@ class ContinuousLinearSender(nn.Module):
     ):
         super(ContinuousLinearSender, self).__init__()
 
+        self.agent = agent
         activations = {"relu": F.relu, "tanh": F.tanh, "leaky_relu": F.leaky_relu, "identity": nn.Identity()}
         self.activation = activations[activation.lower()]
 
         encoder_hidden_sizes = [encoder_hidden_size] * num_layers
         encoder_layer_dimensions = [(encoder_input_size, encoder_hidden_sizes[0])]
 
-        for i, hidden_size in enumerate(self.encoder_hidden_sizes[1:]):
+        for i, hidden_size in enumerate(encoder_hidden_sizes[1:]):
             hidden_shape = (self.encoder_hidden_sizes[i], hidden_size)
             encoder_layer_dimensions.append(hidden_shape)
 
@@ -37,9 +39,10 @@ class ContinuousLinearSender(nn.Module):
         )
 
     def forward(self, x):
-        for hidden_layer in self.decoder_hidden_layers[:-1]:
+        x = self.agent(x)
+        for hidden_layer in self.encoder_hidden_layers[:-1]:
             x = self.activation(hidden_layer(x))
-        sender_output = self.decoder_hidden_layers[-1](x)
+        sender_output = self.encoder_hidden_layers[-1](x)
         return sender_output
 
 
@@ -63,11 +66,38 @@ class SenderReceiverContinuousCommunication(nn.Module):
         sender: nn.Module,
         receiver: nn.Module,
         loss: Callable,
+        train_logging_strategy: Optional[LoggingStrategy] = None,
+        test_logging_strategy: Optional[LoggingStrategy] = None,
     ):
+        """
+        :param sender: Sender agent. sender.forward() has to output a continouos vector
+        :param receiver: Receiver agent. receiver.forward() has to accept two parameters:
+            message and receiver_input.
+        `message` is shaped as (batch_size, vocab_size).
+        :param loss: Callable that outputs differentiable loss, takes the following parameters:
+          * sender_input: input to Sender (comes from dataset)
+          * message: message sent from Sender
+          * receiver_input: input to Receiver from dataset
+          * receiver_output: output of Receiver
+          * labels: labels that come from dataset
+        :param train_logging_strategy, test_logging_strategy: specify what parts of interactions to persist for
+            later analysis in the callbacks.
+        """
         super(SenderReceiverContinuousCommunication, self).__init__()
         self.sender = sender
         self.receiver = receiver
         self.loss = loss
+
+        self.train_logging_strategy = (
+            LoggingStrategy()
+            if train_logging_strategy is None
+            else train_logging_strategy
+        )
+        self.test_logging_strategy = (
+            LoggingStrategy()
+            if test_logging_strategy is None
+            else test_logging_strategy
+        )
 
     def forward(self, sender_input, labels, receiver_input=None):
         message = self.sender(sender_input)
@@ -76,5 +106,17 @@ class SenderReceiverContinuousCommunication(nn.Module):
         loss, aux_info = self.loss(
             sender_input, message, receiver_input, receiver_output, labels
         )
+        logging_strategy = (
+            self.train_logging_strategy if self.training else self.test_logging_strategy
+        )
 
-        return loss.mean(), Interaction.empty()
+        interaction = logging_strategy.filtered_interaction(
+            sender_input=sender_input,
+            receiver_input=receiver_input,
+            labels=labels,
+            receiver_output=receiver_output,
+            message=message.detach(),
+            message_length=torch.ones(message[0].size(0)),
+            aux=aux_info,
+        )
+        return loss.mean(), interaction
