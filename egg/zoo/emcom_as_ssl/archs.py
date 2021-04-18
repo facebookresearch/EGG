fix game_acc computation and remove post_hoc evalution

egg/zoo/emcom_as_ssl/archs.py
@@ -67,7 +67,10 @@ class VisionModule(nn.Module):
 
     def forward(self, x_i, x_j):
         encoded_input_sender = self.encoder(x_i)
-        encoded_input_recv = self.encoder(x_j) if self.shared else self.encoder_recv(x_j)
+        if self.shared:
+            encoded_input_recv = self.encoder(x_j)
+        else:
+            encoded_input_recv = self.encoder_recv(x_j)
         return encoded_input_sender, encoded_input_recv
 
 
@@ -88,7 +91,7 @@ class VisionGameWrapper(nn.Module):
         return self.game(
             sender_input=sender_encoded_input,
             labels=labels,
-            receiver_input=receiver_encoded_input,
+            receiver_input=receiver_encoded_input
         )
 
 

egg/zoo/emcom_as_ssl/losses.py
@@ -128,7 +128,7 @@ class NTXentLoss(Loss):
 
         acc = (torch.argmax(logits.detach(), dim=1) == labels).float().detach()  # this is soft_acc
 
-        logits_msg_img = negative_samples[:batch_size, batch_size:]
+        logits_msg_img = similarity_matrix[:batch_size, batch_size:]
         labels_msg_img = torch.arange(batch_size, device=message.device)
         game_acc = (torch.argmax(logits_msg_img, dim=1) == labels_msg_img).float().detach()
 