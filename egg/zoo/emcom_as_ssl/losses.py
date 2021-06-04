a9f6665c6831200ba6642f4aa1755f6e773e6cd7
Roberto <roberto.dessi11@gmail.com>
2021-06-04 02:19:33 +0200

    Adding new communication game where cnn is trained from scratch (#206)
    * rl wip
    
    * add models
    
    * discri almost done
    
    * disc game working
    
    * clean
    
    * First draft for SimCLR game
    
    * first draft contrastive_loss shared_vision_module
    
    * stable cont game
    
    * cont_comm
    
    * stable rf and cont simclr game
    
    * sweeps
    
    * multi gpu support
    
    * avoid multiple logging with distributed training
    
    * add test simclr does not fail and upd
    
    * add find_unused_paramter in ddp instance for distributed training support with reinforce
    
    * fix consoleLogger
    
    * fix simclr test and updated sweeps
    
    * new search
    
    * no_aug searches
    
    * add support for validation loop
    
    * implement validation loop and optional no-nonlinear transf of img feats
    
    * fix augmentation and add communication game
    
    * add contrastive-only and communication-only games
    
    * remove validation and fix vision_module saver
    
    * fixes
    
    * add optimizer scheduler
    
    * upd
    
    * upd
    
    * add simclr game
    
    * upd
    
    * add cifar10
    
    * upd communication game
    
    * add simclr in separate folder and revert changes to consolelogger
    
    * upd
    
    * upd
    
    * upd
    
    * add LARC and fix import
    
    * upd
    
    * upd
    
    * gs-only game
    
    * gs working
    
    * fix sweeps
    
    * upd
    
    * upd
    
    * upd
    
    * sweeps
    
    * upd
    
    * gs exps
    
    * upd
    
    * upd
    
    * remove early stopper
    
    * store train interactions
    
    * add interaction logging frequency
    
    * upd
    
    * add simclr_replica
    
    * add gaussian noise and storing intermediate repr
    
    * add sweeps
    
    * simclr_replica mem opt
    
    * change dir namne
    
    * joined simclr em_comm_ssl game
    
    * done refactoring
    
    * done refactoring
    
    * bug fix loss type
    
    * bug fix
    
    * fix parameter and double label in loss
    
    * stable game
    
    * log game accuracy and add sweep
    
    * upd
    
    * fix sender fc
    
    * upd
    
    * add sweeps and remove normalization
    
    * fix game_acc computation and remove post_hoc evalution
    
    * fix xent loss
    
    * fix shared vision module stop grad
    
    * fix interaction saving for post_hoc eval
    
    * fix o_test dataset path
    
    * fix xent loss
    
    * use fp32 for aug_no_shared models
    
    * remove gaussion eval during training
    
    * add analysis scripts and option for argmax simclr
    
    * add analysis scripts and option for argmax simclr
    
    * post-analysis scripts
    
    * add optional flags
    
    * fix interaction saving
    
    * remove validation loop and add playground sweeps
    
    * fix game callbacks
    
    * upd analysis scripts
    
    * visualize nbook and game sweep
    
    * visualize nbook and game sweep
    
    * add simclr sweeps
    
    * fix simclr
    
    * fix arch
    
    * remove unused files
    
    * polish
    
    * neurips version
    
    * clean v1
    
    * remove wandb
    
    * remove unused wandb
    
    * add ood test set
    
    * fix scikit learn import
    
    * upd
    
    * change timestamp resolution when saving checkpoint
    
    * fix epoch counter
    
    * upd
    
    * upd
    
    Co-authored-by: Jade Copet <jadecopet@fb.com>

---
+++ egg/zoo/emcom_as_ssl/losses.py
@@ -0,0 +1,101 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def get_loss(
+    temperature: float = 1.0,
+    similarity: str = "cosine",
+    loss_type: str = "xent"
+):
+    if loss_type.lower() == "xent":
+        return XEntLoss(temperature=temperature, similarity=similarity)
+    elif loss_type.lower() == "ntxent":
+        return NTXentLoss(temperature=temperature, similarity=similarity)
+    else:
+        raise NotImplementedError(f"ERROR: cannot recognize {loss_type} loss")
+
+
+class Loss:
+    def __init__(
+            self,
+            temperature: float = 1.0,
+            similarity: str = "cosine"
+    ):
+        self.temperature = temperature
+
+        similarities = {"cosine", "dot"}
+        assert similarity.lower() in similarities, "Cannot recognize similarity function {similarity}"
+        self.similarity = similarity
+
+    def get_similarity_matrix(
+        self,
+        message: torch.Tensor,
+        receiver_output: torch.Tensor,
+        similarity: str = "cosine"
+    ):
+        if self.similarity == "cosine":
+            similarity_f = nn.CosineSimilarity(dim=2)
+            return similarity_f(message.unsqueeze(1), receiver_output.unsqueeze(0)) / self.temperature
+        elif self.similarity == "dot":
+            return message @ receiver_output.t()
+
+
+class XEntLoss(Loss):
+    def xent_loss(self, message: torch.Tensor, receiver_output: torch.Tensor):
+        batch_size = receiver_output.shape[0]
+        model_guesses = self.get_similarity_matrix(message, receiver_output)
+
+        labels = torch.arange(batch_size, device=message.device)
+        acc = (model_guesses.argmax(dim=1) == labels).detach().float()
+        loss = F.cross_entropy(model_guesses, labels, reduction="none")
+        return loss, {"acc": acc, "game_acc": acc}
+
+    def __call__(self, _sender_input, message, _receiver_input, receiver_output, _labels):
+        assert message.shape == receiver_output.shape, "Message and receiver output must be of the same size."
+        return self.xent_loss(message, receiver_output)
+
+
+class NTXentLoss(Loss):
+    def ntxent_loss(self, message, receiver_output):
+        batch_size = message.shape[0]
+
+        input = torch.cat((message, receiver_output), dim=0)
+
+        similarity_matrix = self.get_similarity_matrix(input, input)
+
+        sim_i_j = torch.diag(similarity_matrix, batch_size)
+        sim_j_i = torch.diag(similarity_matrix, -batch_size)
+
+        positive_samples = torch.cat(
+            (sim_i_j, sim_j_i),
+            dim=0
+        ).reshape(batch_size * 2, 1)
+
+        mask = torch.ones(
+            (batch_size * 2, batch_size * 2),
+            dtype=bool
+        ).fill_diagonal_(0)
+
+        negative_samples = similarity_matrix[mask].reshape(batch_size * 2, -1)
+
+        labels = torch.zeros(batch_size * 2).to(positive_samples.device).long()
+        logits = torch.cat((positive_samples, negative_samples), dim=1)
+
+        loss = F.cross_entropy(logits, labels, reduction="none") / 2
+
+        acc = (torch.argmax(logits.detach(), dim=1) == labels).float().detach()  # this is soft_acc
+
+        logits_msg_img = similarity_matrix[:batch_size, batch_size:]
+        labels_msg_img = torch.arange(batch_size, device=message.device)
+        game_acc = (torch.argmax(logits_msg_img, dim=1) == labels_msg_img).float().detach()
+
+        return loss, {"acc": acc, "game_acc": game_acc}
+
+    def __call__(self, _sender_input, message, _receiver_input, receiver_output, _labels):
+        return self.ntxent_loss(message, receiver_output)
