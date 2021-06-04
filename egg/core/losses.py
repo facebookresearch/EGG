158208f6315929dae76ab66b8c054bbb08ae0ba1
robertodessi <roberto.dessi11@gmail.com>
2021-06-04 06:46:16 -0700

    fix ntxent loss

--- egg/core/losses.py
+++ egg/core/losses.py
@@ -53,12 +53,13 @@ class ReconstructionLoss:
 
 
 class NTXentLoss:
+    """NTXentLoss used in https://arxiv.org/abs/2002.05709."""
+
     def __init__(
             self,
             batch_size: int,
-            temperature: float = 0.1,
+            temperature: float,
             similarity: str = "cosine",
-            normalize_before: bool = True
     ):
         self.temperature = temperature
         self.batch_size = batch_size
@@ -67,12 +68,10 @@ class NTXentLoss:
         assert similarity.lower() in similarities, f"Cannot recognize similarity function {similarity}"
         self.similarity = similarity
 
-        self.normalized_before = normalize_before
+    def __call__(self, _sender_input, message, _receiver_input, receiver_output, _labels):
+        batch_size = message.shape[0]
 
-    def __call__(self, sender_input, _message, _receiver_input, receiver_output, _labels):
-        input = receiver_output
-        if self.normalize_before:
-            input = F.normalize(input, dim=1)
+        input = torch.cat((message, receiver_output), dim=0)
 
         if self.similarity == "cosine":
             similarity_f = nn.CosineSimilarity(dim=2)
@@ -80,27 +79,25 @@ class NTXentLoss:
         elif self.similarity == "dot":
             similarity_matrix = input @ input.t()
 
-        N = 2 * self.batch_size
+        sim_i_j = torch.diag(similarity_matrix, batch_size)
+        sim_j_i = torch.diag(similarity_matrix, -batch_size)
 
-        sim_i_j = torch.diag(similarity_matrix, self.batch_size)
-        sim_j_i = torch.diag(similarity_matrix, -self.batch_size)
+        positive_samples = torch.cat(
+            (sim_i_j, sim_j_i),
+            dim=0
+        ).reshape(batch_size * 2, 1)
 
-        positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(
-            N, 1
-        )
+        mask = torch.ones(
+            (batch_size * 2, batch_size * 2),
+            dtype=bool
+        ).fill_diagonal_(0)
 
-        mask = torch.ones((N, N), dtype=bool)
-        mask = mask.fill_diagonal_(0)
-        for i in range(self.batch_size):
-            mask[i, self.batch_size + i] = 0
-            mask[self.batch_size + i, i] = 0
+        negative_samples = similarity_matrix[mask].reshape(batch_size * 2, -1)
 
-        negative_samples = similarity_matrix[mask].reshape(N, -1)
-
-        labels = torch.zeros(N).to(positive_samples.device).long()
+        labels = torch.zeros(batch_size * 2).to(positive_samples.device).long()
         logits = torch.cat((positive_samples, negative_samples), dim=1)
-        loss_ij = F.cross_entropy(logits[:self.batch_size], labels[:self.batch_size], reduction="none")
-        loss_ji = F.cross_entropy(logits[self.batch_size:], labels[self.batch_size:], reduction="none")
-        loss = (loss_ij + loss_ji) / 2
-        acc = (torch.argmax(logits, dim=1) == labels).float()
+
+        loss = F.cross_entropy(logits, labels, reduction="none") / 2
+
+        acc = (torch.argmax(logits.detach(), dim=1) == labels).float().detach()
         return loss, {"acc": acc}
