9d1b0faad6c553edd02cfca20d096688fffe2870
Aga Slowik <aslowik@fb.com>
2021-07-22 17:49:51 +0100

    vocab size in the Gaussian test

--- egg/zoo/population_game/scripts/gaussian_noise_analysis.py
+++ egg/zoo/population_game/scripts/gaussian_noise_analysis.py
@@ -62,6 +62,7 @@ def main():
     opts = get_params(
         n_senders=cli_args.n_senders,
         n_recvs=cli_args.n_recvs,
+        vocab_size=cli_args.vocab_size,
     )
 
     if cli_args.pdb:
