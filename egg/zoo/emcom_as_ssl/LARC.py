e1e187d1ee162c4ff7d3ded56d467e7954048c01
Roberto <rdessi@fb.com>
2021-10-15 10:56:56 +0200

    Update LARC.py

--- egg/zoo/emcom_as_ssl/LARC.py
+++ egg/zoo/emcom_as_ssl/LARC.py
@@ -1,15 +1,23 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-import torch
-
-
 #  Original code from https://github.com/NVIDIA/apex/blob/master/apex/parallel/LARC.py,
 #  copied here to drop apex dependency and to customize it. When copying optimizer state to gpu,
 #  the lack of an explicit state setter method caused and AttributeError.
 #  Fixed it by adding the @state.setter-decorated method
+
+# Please note that the code below is released under the following license:
+
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import torch
+
+
 class LARC:
     """
     :class:`LARC` is a pytorch implementation of both the scaling and clipping variants of LARC,
