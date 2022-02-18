d00187e65daa7b3bcb658b4b395937f45c86e59b
mahautm <mmahaut@ensc.fr>
2022-02-18 11:02:17 +0100

    smaller batch

--- egg/zoo/pop/scripts/sequential_queue_tool.py
+++ egg/zoo/pop/scripts/sequential_queue_tool.py
@@ -12,11 +12,17 @@ def write_sequential_jobs(
     logfile_path="/mnt/efs/fs1/logs/",
     prefix="incep_hp_search",
     command_path="~",
+    gpu=None,
 ):  # doesn't work because of wandb
     with open(command_path, "w") as command_file:
         with open(json_path, "r") as f:
             params = json.load(f)
             allkeys = sorted(params)
+            cuda = ""
+            if gpu is None:
+                cuda = ""
+            else:
+                cuda = f"CUDA_VISIBLE_DEVICES={gpu}"
             experiments = it.product(*(params[key] for key in allkeys))
             for experiment in experiments:
                 args = []
@@ -46,18 +52,21 @@ def write_sequential_jobs(
                 if not os.path.exists(outdir):
                     os.mkdir(outdir)
                 command_file.write(
-                    f"sudo python3 egg/zoo/pop/train.py {' '.join(args)} \n"
+                    f"sudo {cuda} python3 egg/zoo/pop/train.py {' '.join(args)} \n"
                 )
 
 
 if __name__ == "__main__":
     wandb.config.allow_val_change = True
     args = sys.argv[1:]
+    # [0] is arch type
+    # [1] is gpu number
     command_file_path = f"../bash_exp_plans/hp_{args[0]}.sh"
     write_sequential_jobs(
         f"/mnt/efs/fs1/EGG/egg/zoo/pop/paper_sweeps/{args[0]}_hp_search.json",
         prefix=f"{args[0]}_hp_search_p",
         command_path=command_file_path,
+        gpu=None if len(args) == 1 else args[1],
     )
     print(f"experimental plan written to {command_file_path}, launching...")
     os.system(f"sudo bash {command_file_path}")
