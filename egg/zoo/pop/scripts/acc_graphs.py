4e0ae8c095e3ec2990eb7d9fd4dc8d9ca2731bb2
mahautm <mmahaut@ensc.fr>
2022-02-21 11:04:04 +0100

    very verbose

--- egg/zoo/pop/scripts/acc_graphs.py
+++ egg/zoo/pop/scripts/acc_graphs.py
@@ -69,7 +69,7 @@ def check_constraints(path, names=[], values=[], verbose=False):
                 f, "wandb" if path[len(path) - 4 : len(path)] == "json" else "nest"
             )
             for i in range(len(values)):
-                if not extract_param(names[i], params) in values[i]:
+                if not extract_param(names[i], params, verbose=True) in values[i]:
                     respects_constraints = False
     if verbose:
         print(f"constraints respected : {respects_constraints}")
