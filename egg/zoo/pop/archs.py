545b763a1de87a2e5a8ca8370cad5f4859f85d5b
mahautm <mmahaut@ensc.fr>
2022-03-30 12:13:12 +0200

    quick debu

--- egg/zoo/pop/archs.py
+++ egg/zoo/pop/archs.py
@@ -276,6 +276,7 @@ class PopulationGame(nn.Module):
     def forward(self, *args, **kwargs):
         sender, receiver, loss, idxs = self.agents_loss_sampler()
         sender_idx, recv_idx, loss_idx = idxs
+        print(kwargs, flush=True)
         # creating an aux_input
         args = list(args)
         args[-1] = {
