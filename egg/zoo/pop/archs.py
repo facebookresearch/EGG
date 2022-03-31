3e512b8d0695b27a0f9ec4e71f98f089f90af8ff
mahautm <mmahaut@ensc.fr>
2022-03-31 16:13:50 +0200

    solving for memory

--- egg/zoo/pop/archs.py
+++ egg/zoo/pop/archs.py
@@ -273,20 +273,20 @@ class PopulationGame(nn.Module):
         self.game = game
         self.agents_loss_sampler = agents_loss_sampler
 
-    def forward(self, *args, **kwargs):
+
+    def forward(self, device='cuda',*args, **kwargs):
         sender, receiver, loss, idxs = self.agents_loss_sampler()
         sender_idx, recv_idx, loss_idx = idxs
         # creating an aux_input
-        # sender and receiver input to CUDA
-        args = [
-            arg.to("cuda") if arg is not None else arg for arg in args
-        ]  # list(args)
+        sender.to(device)
+        receiver.to(device)
+        args = [arg.to("cuda") if arg is not None else arg for arg in args] # list(args)
         args[-1] = {
             "sender_idx": sender_idx,
             "recv_idx": recv_idx,
             "loss_idx": loss_idx,
         }
-        mean_loss, interactions = self.game(
-            sender.to("cuda"), receiver.to("cuda"), loss, *args, **kwargs
-        )
-        return mean_loss, interactions
+        mean_loss, interactions = self.game(sender, receiver, loss, *args, **kwargs)
+        sender.to('cpu')
+        receiver.to('cpu')
+        return mean_loss.to('cpu'), interactions #sent to cpu in trainer
