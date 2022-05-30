a54a402c7a962c1c46a103906dd8de7f17aa8d6f
mahautm <mmahaut@ensc.fr>
2022-05-30 17:18:45 +0200

    this maybe should be in the working branch : updated the graph tool

--- egg/zoo/pop/scripts/graph_tools/ls_graphs.py
+++ egg/zoo/pop/scripts/graph_tools/ls_graphs.py
@@ -5,7 +5,7 @@ def ls_graph(
     verbose=False,
     save_path="/shared/mateo/logs/",
     graph_name="arch_graph",
-    graph_title="no_vgg",
+    graph_title="no_vit",
 ):
     """
     params
@@ -29,8 +29,8 @@ def ls_graph(
             # "additional_receiver",
         ],
         values=[
-            [["inception", "vit", "resnet152"]],
-            [["inception", "vit", "resnet152"]],
+            [["vgg11", "inception", "resnet152"]],
+            [["vgg11", "inception", "resnet152"]],
         ],
         verbose=verbose,
         label_names=["additional_sender", "additional_receiver"],
