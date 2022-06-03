7df4c408bd333aadddec9ce27125d18bea431890
mahautm <mmahaut@ensc.fr>
2022-06-03 16:17:04 +0200

    summary tool

--- egg/zoo/pop/scripts/graph_tools/list_overview.py
+++ egg/zoo/pop/scripts/graph_tools/list_overview.py
@@ -6,11 +6,13 @@ from egg.zoo.pop.scripts.graph_tools.acc_graphs import metadata_opener, extract_
 def list_results(path="/shared/mateo/logs/continuous_hyp_2/",param_names=['vision_model_names_recvs','non_linearity','force_gumbel','vocab_size'], epoch_numbers=[1,25]):
     file_paths = glob.glob(path + "*.out")
     for file_path in file_paths:
-        # we're looking for this :
         with open(file_path) as file:
             # lets get reading
-            params = [extract_param(param, metadata_opener(file, 'nest')) for param in param_names]
-            accuracies = [text_to_acc(file)[1][i-1] for i in epoch_numbers]
-            print(accuracies, params)
+            results = [extract_param(param, metadata_opener(file, 'nest')) for param in param_names]
+            x, accs = text_to_acc(file)
+            if len(accs) != 0:
+                for n in epoch_numbers:
+                    results.append(accs[n-1])
+            print(results)
             
                     
