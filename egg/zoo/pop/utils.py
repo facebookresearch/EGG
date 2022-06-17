eadaf521d598135847b06608452e7fe301cb42ab
mahautm <mateo.mahaut@upf.edu>
2022-06-17 14:02:09 +0200

    gaussian blur dataset

--- egg/zoo/pop/utils.py
+++ egg/zoo/pop/utils.py
@@ -22,7 +22,7 @@ def get_data_opts(parser):
 
     group.add_argument(
         "--dataset_name",
-        choices=["cifar100", "imagenet"],
+        choices=["cifar100", "imagenet", "gaussian_noise"],
         default="imagenet",
         help="Dataset used for training a model",
     )
