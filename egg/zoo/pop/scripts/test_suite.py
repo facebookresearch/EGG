05bb477d5f41b4b73ab51044d2cc238ccb70cf19
mahautm <mateo.mahaut@upf.edu>
2022-06-22 17:24:14 +0200

    some continuous analysis clean up and preparing data_augmentation experiment

---
+++ egg/zoo/pop/scripts/test_suite.py
@@ -0,0 +1,7 @@
+# a test to surviving noise
+# a test to surviving adversarial attacks
+# a test to surviving data augmentation
+# (also... the out of scope data test ? is there new non gaussian stuff it could be good at ?)
+# a test to playing with discretised messages post-training
+
+# all of this on het vs hom languages
