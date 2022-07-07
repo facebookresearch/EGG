b4119782b50712f9b068cfcbb777a69279fd77e8
mahautm <mateo.mahaut@upf.edu>
2022-07-07 16:50:22 +0200

    debug automatic parameter choice

--- egg/zoo/pop/scripts/analysis_tools/extract_com.py
+++ egg/zoo/pop/scripts/analysis_tools/extract_com.py
@@ -26,7 +26,7 @@ def main(params):
 
     f = open(path_to_parameters(_path))
     opts = get_common_opts(metadata_opener(f, data_type="nest", verbose=True) + params)
-
+    print(opts)
     build_and_test_game(opts, exp_name=str(opts.augmentation_type) + str(opts.vision_model_names_senders) + str(opts.vision_model_names_recvs), dump_dir=opts.checkpoint_dir)
 
 
