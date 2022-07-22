66bb04f4a7b34c6af85aa70200529b288e6591af
mahautm <mateo.mahaut@upf.edu>
2022-07-22 16:29:39 +0200

    bypassing missing nest/submitit

--- egg/zoo/pop/scripts/sweeper.py
+++ egg/zoo/pop/scripts/sweeper.py
@@ -2,13 +2,15 @@ import os
 import json
 import itertools as it
 import sys
-def sweep_params(params_path,jobname="job",partition="alien",n_gpus=1,time="3-00:00:00",mem="8G"):
+def sweep_params(params_path,jobname="job", sbatch_dir="/homedtcl/mmahaut/projects/manual_slurm", partition="alien",n_gpus=1,time="3-00:00:00",mem="8G"):
     with open(params_path, "r") as f:
         params = json.load(f)
         for _l in it.product(*(params[key] for key in params)):
             command=build_command(_l, params.keys())
             write_sbatch(command,jobname,partition,n_gpus,time,mem)
-            os.system(f"sbatch {jobname}.sh")
+            sbatch_file = os.path.join(sbatch_file, f"{jobname}.sh")
+            os.system(f"sbatch {sbatch_file}")
+            
 
 def build_command(params, keys):
     command=f"python -m egg.zoo.pop.train "
@@ -22,11 +24,11 @@ def build_command(params, keys):
     return command
 
 
-def write_sbatch(command,jobname,partition="alien",n_gpus=1,time="3-00:00:00",mem="8G"):
+def write_sbatch(command,jobname,sbatch_dir="/homedtcl/mmahaut/projects/manual_slurm",partition="alien",n_gpus=1,time="3-00:00:00",mem="8G"):
     """
     writes a sbatch file for the current job
     """
-    sbatch_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{jobname}.sh")
+    sbatch_path = os.path.join(os.path.dirname(sbatch_dir, f"{jobname}.sh")
     with open(sbatch_path, "w") as f:
         f.write(
             f"""#!/bin/bash
@@ -48,4 +50,4 @@ echo "done"
 
 if __name__ == "__main__":
     # this is not as clean as something like itertools, but it works for now
-    sweep_params(params_path=sys.argv[1], jobname=sys.argv[2] if len(sys.argv)>2 else "job",partition=sys.argv[3] if len(sys.argv)>3 else "alien",n_gpus=sys.argv[4] if len(sys.argv)>4 else 1,time=sys.argv[5] if len(sys.argv)>5 else "3-00:00:00",mem=sys.argv[6] if len(sys.argv)>6 else "8G")
+    sweep_params(params_path=sys.argv[1], jobname=sys.argv[2] if len(sys.argv)>2 else "job", sbatch_dir=sys.argv[3] if len(sys.argv)>3 else "/homedtcl/mmahaut/projects/manual_slurm", partition=sys.argv[4] if len(sys.argv)>4 else "alien",n_gpus=sys.argv[5] if len(sys.argv)>5 else 1,time=sys.argv[6] if len(sys.argv)>6 else "3-00:00:00",mem=sys.argv[7] if len(sys.argv)>7 else "8G")
