48e19c09857b35fa320ba1fc0383a3ce71dd6560
mahautm <mateo.mahaut@upf.edu>
2022-07-27 10:42:05 +0200

    imagenet correction

--- egg/zoo/pop/data.py
+++ egg/zoo/pop/data.py
@@ -192,7 +192,7 @@ class ImageTransformation:
                     transforms.Normalize(
                         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
                     ),
-                    transforms.CenterCrop(299),
+                    transforms.CenterCrop(size),
                 ]
             )
         elif dataset_name in ["cifar100", "inaturalist"]:
