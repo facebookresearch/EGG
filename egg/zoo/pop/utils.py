5ea64fbc62841be4472fb408a98a8075d4cccdc7
mahautm <mateo.mahaut@upf.edu>
2022-08-16 16:56:24 +0200

    temp_fix

--- egg/zoo/pop/utils.py
+++ egg/zoo/pop/utils.py
@@ -298,8 +298,8 @@ def add_weight_decay(model, weight_decay=1e-5, skip_name=""):
 def path_to_parameters(path):
     old_game = pathlib.Path(path)
     job_number = str(old_game.parents[0].stem)
-    all_out_files = [f for f in old_game.parents[1].glob(f"*{job_number}*.out")]
-    assert len(all_out_files) == 1, f"Found more than one out file : {all_out_files}"
+    all_out_files = [f for f in old_game.parents[1].glob(f"*{job_number}.out")]
+    assert len(all_out_files) == 1, f"did not find one out file (missing or duplicates) : {all_out_files}"
     return all_out_files[0]
 
 def metadata_opener(file, data_type: str, verbose=False):
