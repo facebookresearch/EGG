01e050bed0fab62ebc66d849a1535a68f0395313
mahautm <mateo.mahaut@upf.edu>
2022-08-22 10:43:45 +0200

    retraining models on partial imagenet

---
+++ egg/zoo/pop/scripts/imagenet_subsets.py
@@ -0,0 +1,119 @@
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+# PyTorch TensorBoard support
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+import timm
+from egg.zoo.pop.data import get_dataloader
+
+def initialize_vision_module(name: str = "resnet50", pretrained: bool = False, aux_logits=True):
+    print("initialize module", name)
+    # TODO : Matéo this could use some lazyloading instead of loading them all even if they're not being used
+    # It also reloads all of them every time we pick one !
+    modules = {
+        "resnet50": torchvision.models.resnet50(pretrained=pretrained),
+        "resnet101": torchvision.models.resnet101(pretrained=pretrained),
+        "resnet152": torchvision.models.resnet152(pretrained=pretrained),
+        "inception": torchvision.models.inception_v3(
+            pretrained=pretrained, aux_logits=aux_logits
+        ),
+        "vgg11": torchvision.models.vgg11(pretrained=pretrained),
+        "vit": timm.create_model("vit_base_patch16_384", pretrained=pretrained),
+        "swin":timm.create_model("swin_base_patch4_window12_384", pretrained=pretrained),
+        "dino":torch.hub.load('facebookresearch/dino:main', 'dino_vits16',verbose=False)
+    }
+
+    return modules[name]
+
+def train_one_epoch(epoch_index, tb_writer):
+    running_loss = 0.
+    last_loss = 0.
+
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    for i, data in enumerate(training_loader):
+        # Every data instance is an input + label pair
+        inputs, labels = data
+
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+
+        # Make predictions for this batch
+        outputs = model(inputs)
+
+        # Compute the loss and its gradients
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+
+        # Adjust learning weights
+        optimizer.step()
+
+        # Gather data and report
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            last_loss = running_loss / 1000 # loss per batch
+            print('  batch {} loss: {}'.format(i + 1, last_loss))
+            tb_x = epoch_index * len(training_loader) + i + 1
+            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+            running_loss = 0.
+
+    return last_loss
+
+
+if __name__ == "__main__":
+    validation_loader, training_loader = get_dataloader(dataset_dir="/datasets/COLT/imagenet21k_resized" ,dataset_name="alive_imagenet", batch_size=64, num_workers=4, seed=111, validation_split=0.1)
+
+    model = initialize_vision_module(name="vit", pretrained=False)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+    # Initializing in a separate cell so we can easily add more epochs to the same run
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
+    epoch_number = 0
+
+    EPOCHS = 5
+
+    best_vloss = 1_000_000.
+
+    for epoch in range(EPOCHS):
+        print('EPOCH {}:'.format(epoch_number + 1))
+
+        # Make sure gradient tracking is on, and do a pass over the data
+        model.train(True)
+        avg_loss = train_one_epoch(epoch_number, writer)
+
+        # We don't need gradients on to do reporting
+        model.train(False)
+
+        running_vloss = 0.0
+        for i, vdata in enumerate(validation_loader):
+            vinputs, vlabels = vdata
+            voutputs = model(vinputs)
+            vloss = loss_fn(voutputs, vlabels)
+            running_vloss += vloss
+
+        avg_vloss = running_vloss / (i + 1)
+        print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+
+        # Log the running loss averaged per batch
+        # for both training and validation
+        writer.add_scalars('Training vs. Validation Loss',
+                        { 'Training' : avg_loss, 'Validation' : avg_vloss },
+                        epoch_number + 1)
+        writer.flush()
+
+        # Track best performance, and save the model's state
+        if avg_vloss < best_vloss:
+            best_vloss = avg_vloss
+            model_path = 'model_{}_{}'.format(timestamp, epoch_number)
+            torch.save(model.state_dict(), model_path)
+
+        epoch_number += 1
+
+
