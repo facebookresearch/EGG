97f42f737fe17aa5f1f14f48f7ab1e0264307080
mahautm <mateo.mahaut@upf.edu>
2022-09-13 16:29:57 +0200

    discrete com extraction - adding a different name per experinet

--- egg/zoo/pop/scripts/analysis_tools/extract_com.py
+++ egg/zoo/pop/scripts/analysis_tools/extract_com.py
@@ -28,7 +28,8 @@ def main(params):
     f = open(path_to_parameters(_path, "wandb"))
     opts = get_common_opts(metadata_opener(f, data_type="wandb", verbose=True) + params)
     print(opts)
-    build_and_test_game(opts, exp_name=str(opts.dataset_name) + str(opts.vision_model_names_senders) + str(opts.vision_model_names_recvs), dump_dir=opts.checkpoint_dir)
+    exp_name = str(opts.dataset_name) + str(opts.augmentation_type) + str(opts.vision_model_names_senders) + str(opts.vision_model_names_recvs)
+    build_and_test_game(opts, exp_name=exp_name, dump_dir=opts.checkpoint_dir)
 
 #
 def eval(
