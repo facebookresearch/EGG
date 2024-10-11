2f2fc82bff1afc56280679ad79cf56ec2b8838e1
Cezary Klamra <cklamra@icloud.com>
2024-10-11 19:46:45 +0200

    new data export (5 samples per concept)

--- ancm/export_visa.py
+++ ancm/export_visa.py
@@ -11,33 +11,40 @@ for file in os.listdir(directory):
         continue
     tree = ET.parse(os.path.join(directory, file))
 
+    concept_category = tree.getroot().attrib['category']
     for concept in tree.getroot().iter('concept'):
-        concept_dict = defaultdict(int)
+        attribute_dict = defaultdict(int)
         for category in concept:
             attributes = [a.strip() for a in category.text.split()
                           if a.strip()]  # and not a.startswith('beh')]
             for a in attributes:
-                concept_dict[a] = 1
+                attribute_dict[a] = 1
         if concept.attrib['name'].find('_(') == -1:
             concept_name = concept.attrib['name']
         else:
             concept_name = concept.attrib['name'][:concept.attrib['name'].find('_(')]
 
         if concept_name not in concepts and concept_name not in homonyms:
-            concepts[concept_name] = concept_dict
+            concepts[concept_name] = {
+                'category': concept_category,
+                'attributes': attribute_dict,
+            }
         elif concept_name in homonyms:
             print('skipping', concept_name)
         else:
-            del concepts[concept_name]
-            homonyms.append(concept_name)
-            print('deleting', concept_name)
+             del concepts[concept_name]
+             homonyms.append(concept_name)
+             print('deleting', concept_name)
 
-all_attributes = set([k for cd in concepts.values() for k in cd.keys()])
-output_dict = {'concept': list(concepts.keys())}
+all_attributes = set([k for cd in concepts.values() for k in cd['attributes'].keys()])
+output_dict = {
+    'concept': list(concepts.keys()),
+    'category': [cd['category'] for cd in concepts.values()],
+}
 for attribute in all_attributes:
-    output_dict[attribute] = [cd[attribute] for cd in concepts.values()]
+    output_dict[attribute] = [cd['attributes'][attribute] for cd in concepts.values()]
 output = pandas.DataFrame(output_dict)
 output.to_csv('visa.csv', index=False)
 
 print('number of concepts:', len(output))
-print('number of attributes:', len(output.columns) - 1)
+print('number of attributes:', len(output.columns) - 2)
--- ancm/reformat_visa.py
+++ ancm/reformat_visa.py
@@ -1,58 +1,100 @@
 import argparse
 import torch
+import math
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-n", type=int, default=4)
-args = parser.parse_args()
-n_distractors = args.n
 
-def reshape_make_tensor(data, n_distractors, n_features):
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", type=int, default=4)
+    parser.add_argument("-s", type=int, default=5)
+    args = parser.parse_args()
+    n_distractors = args.d
+    n_samples = args.s
+    return n_distractors, n_samples
+
+
+def reshape_make_tensor(data_concepts, n_distractors, n_features, n_samples, data_distractors=None):
+    if data_distractors is None:  # if no df for distractors is provided, use df for concepts
+        data_distractors = data_concepts
+
     labels = []
-    data_tensor = torch.tensor(data)
-    data_reshaped = torch.empty((len(data), n_distractors + 1, n_features), dtype=torch.float32)
-    for item in range(len(data)):
-        # create a list of size of distractors
-        distractor_idx = list(range(0, n_distractors+1))
-        # randomly pick an index for the actual data and remove that from list and save in labels
-        idx = np.random.randint(0,n_distractors+1)
-        distractor_idx.remove(idx)
-        labels.append(idx)
-        # add feature data to tensor at correct idx
-        data_reshaped[item][idx] = data_tensor[item]
+    categories = data_concepts.iloc[:,0]
+    data_concepts = np.array(data_concepts.iloc[:,1:].values, dtype='int')
+    data_reshaped = torch.empty((len(data_concepts)*n_samples, n_distractors + 1, n_features), dtype=torch.float32)
+    for concept_i in range(len(data_concepts)):
+        category = categories.iloc[concept_i]  # data_concepts.iloc[concept_i,0]
+        for sample_j in range(n_samples):
+            target_pos = np.random.randint(0, n_distractors+1)
+            distractor_pos = [i for i in range(n_distractors) if i != target_pos]
+            data_reshaped[concept_i + sample_j, target_pos, :] = torch.tensor(data_concepts[concept_i])
 
-        for distractor in distractor_idx:
-            picked_features = item
-            while picked_features == item:
-                picked_features = np.random.randint(0, len(data))
-            data_reshaped[item][distractor] = data_tensor[picked_features]
+            if sample_j <= math.floor(sample_j / n_samples) \
+                    and len(data_distractors[data_distractors.category == category]) >= math.ceil(1.5 * n_samples): 
+                # if there are sufficiently many concepts in the category,
+                # pick random distractors from the same category
+     
+                distractors = data_distractors[data_distractors.category == category] 
+                distractors = distractors.sample(n=n_distractors).iloc[:, 1:]
+                distractors = np.array(distractors, dtype='int')
+            
+            else:  #  sample_j <= math.floor(sample_j / n_samples):
+               
+                # randomly pick distractors
+                distractors = data_distractors.iloc[:, 1:]  # remove the category column
+                distractors = distractors.sample(n=n_distractors)
+                distractors = np.array(distractors, dtype='int')
+                
+
+            for distractor_k, distractor_pos in enumerate(distractor_pos):
+                if distractor_pos == target_pos:
+                    continue
+                data_reshaped[concept_i + sample_j, distractor_pos] = torch.tensor(distractors[distractor_k])
+
+            labels.append(target_pos)
 
     return data_reshaped, labels
 
-visa = pd.read_csv("visa.csv")
 
-features = visa.iloc[:, 1:].values
+def reformat(n_distractors, n_samples):
+    np.random.seed(42)
+    visa = pd.read_csv("visa.csv")
 
-# we do not really need thos I think
-textlabels = visa.iloc[:, :1].values
+    # features = visa.iloc[:, 1:].values
+    # textlabels = visa.iloc[:, :1].values
+    # categories = visa.iloc[:, 1:2].values
 
-n_features = features.shape[1]
+    features = visa.iloc[:, 1:]
+    textlabels = visa.iloc[:, :1]
+    n_features = features.shape[1] - 1  # exclude the category column
 
-# divide 60% for train, 20% test and valid
-train_features, temp_features, train_textlabels, temp_labels = train_test_split(features, textlabels, test_size=0.4)
-valid_features, test_features, valid_textlabels, test_textlabels = train_test_split(temp_features, temp_labels, test_size=0.5)
+    # divide 60% for train, 20% test and valid
+    train_features, temp_features, train_textlabels, temp_labels = train_test_split(features, textlabels, test_size=0.4)
+    valid_features, test_features, valid_textlabels, test_textlabels = train_test_split(temp_features, temp_labels, test_size=0.5)
 
-train_size = len(train_features)
-valid_size = len(valid_features)
-test_size = len(test_features)
+    train_size = len(train_features)
+    valid_size = len(valid_features)
+    test_size = len(test_features)
 
-train, train_labels = reshape_make_tensor(train_features, n_distractors, n_features)
-valid, valid_labels = reshape_make_tensor(valid_features, n_distractors, n_features)
-test, test_labels = reshape_make_tensor(test_features, n_distractors, n_features)
+    train, train_labels = reshape_make_tensor(train_features, n_distractors, n_features, n_samples, features)
+    valid, valid_labels = reshape_make_tensor(valid_features, n_distractors, n_features, n_samples, features)
+    test, test_labels = reshape_make_tensor(test_features, n_distractors, n_features, n_samples, features)
+   
+    print('train:', len(train_labels))
+    print('val:', len(valid_labels))
+    print('test:', len(test_labels))
 
-np.savez(f"visa-{n_distractors+1}", train=train, valid=valid, test=test,
-         train_labels=train_labels, valid_labels=valid_labels, test_labels=test_labels,
-         n_distractors=n_distractors)
-_distractors = args.n
+    np.savez(f"visa-{n_distractors+1}", train=train, valid=valid, test=test,
+             train_labels=train_labels, valid_labels=valid_labels, test_labels=test_labels,
+             n_distractors=n_distractors)
+
+
+def main():
+    n_distractors, n_samples = parse_args()
+    reformat(n_distractors, n_samples)
+
+
+if __name__ == '__main__':
+    main()
