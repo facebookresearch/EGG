f63def3a2ca176898533755915a69e26fa0533e1
Cezary Klamra <cklamra@icloud.com>
2024-10-15 07:53:26 +0200

    cleaning up

--- ancm/custom_callbacks.py
+++ ancm/custom_callbacks.py
@@ -26,7 +26,7 @@ class EpochProgress(Progress):
     class CompletedColumn(ProgressColumn):
         def render(self, task):
             """Calculate common unit for completed and total."""
-            download_status = f"{int(task.completed)}/{int(task.total)} btc"
+            download_status = f"{int(task.completed)}/{int(task.total)} ep"
             return Text(download_status, style="progress.download")
 
     class TransferSpeedColumn(ProgressColumn):
@@ -83,17 +83,27 @@ class CustomProgressBarLogger(Callback):
         self.live.start()
 
         self.p = self.progress.add_task(
-            description="[cyan]Training",
+            description="",
             cur_epoch=0,
             n_epochs=self.n_epochs,
             start=True,
             visible=True,
             total=self.n_epochs)
-        self.history = dict()
 
-        self.eval_style = "white on black"
-        self.train_style = "grey37 on black"
+        self.style = {
+            'eval': 'white on black',
+            'train': 'grey37 on black',
+        }
 
+    def build_od(self, logs, loss, epoch, phase):
+        od = OrderedDict()
+        od["epoch"] = epoch
+        od['phase'] = phase
+        od["loss"] = loss
+        aux = {k: float(torch.mean(v)) for k, v in logs.aux.items()}
+        od.update(aux)
+        return od
+ 
     def get_row(self, od, header=False):
         row = Table(expand=True, box=None, show_header=header, show_footer=False)
         for colname in od.keys():
@@ -104,19 +114,16 @@ class CustomProgressBarLogger(Callback):
         if not header:
             row.add_row(
                 str(od.pop('epoch')),
-                *list(v if isinstance(v, str) else f"{v: 4.4f}" for v in od.values()),
-                style=self.train_style if od['phase'] == 'train' else self.eval_style)
+                *list(self.format_metric_val(v) for v in od.values()),
+                style=self.style[od['phase']])
         return row
 
     @staticmethod
-    def build_od(logs, loss, epoch, phase):
-        od = OrderedDict()
-        od["epoch"] = epoch
-        od['phase'] = phase
-        od["loss"] = loss
-        aux = {k: float(torch.mean(v)) for k, v in logs.aux.items()}
-        od.update(aux)
-        return od
+    def format_metric_val(val):
+        if not isinstance(val, str):
+            return f'{val: 4.3f}'
+        else:
+            return val
 
     def generate_live_table(self, od=None):
         live_table = Table.grid(expand=True)
@@ -126,9 +133,6 @@ class CustomProgressBarLogger(Callback):
         live_table.add_row(self.progress)
         return live_table
 
-    def on_epoch_begin(self, epoch: int):
-        pass
-
     def on_epoch_end(self, loss: float, logs: Interaction, epoch: int):
         od = self.build_od(logs, loss, epoch, 'train')
         if self.print_train_metrics:
@@ -137,9 +141,6 @@ class CustomProgressBarLogger(Callback):
             row = self.get_row(od)
             self.console.print(row)
 
-    def on_validation_begin(self, epoch: int):
-        pass
-
     def on_validation_end(self, loss: float, logs: Interaction, epoch: int):
         self.progress.update(self.p, refresh=True, advance=1, cur_epoch=epoch)
 
@@ -147,7 +148,7 @@ class CustomProgressBarLogger(Callback):
         if self.test_data_len == 0:
             self.test_data_len = self.progress.tasks[self.p].completed
 
-        od = self.build_od(logs, loss, epoch, 'eval ')
+        od = self.build_od(logs, loss, epoch, 'eval')
         if not self.print_train_metrics and epoch == 1:
             self.live.update(self.generate_live_table(od))
         row = self.get_row(od)
@@ -155,6 +156,3 @@ class CustomProgressBarLogger(Callback):
 
     def on_train_end(self):
         self.live.stop()
-
-    def on_batch_end(self, logs: Interaction, loss: float, batch_id: int, is_training: bool = True):
-        pass
